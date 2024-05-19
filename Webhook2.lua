82bc085803b6e3f0897a46efc59082bae7cc82f8
ElijahGamingRBLX <elijahgamingbg@gmail.com>
2024-05-19 18:49:41 +0800

    Update Webhook2.lua

--- Webhook2.lua
+++ Webhook2.lua
@@ -1,4 +1,4 @@
-local url = ""
+local url = "https://discord.com/api/webhooks/1241662189681770526/p7nNk5yAgj3OxraqcUH8Wvl3PanokVSmnnteMzMNth_rZ_giTFR4O-VHJPXovWmjve8o"
 local http = game:GetService("HttpService")
 local webhook = require(script.Parent.WebhookService)
 
